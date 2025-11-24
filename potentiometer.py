2f4c734f21c7e48617af2a8fb5458fd63e1d3011
cjwright <callumjwright@gmail.com>
2025-11-24 17:34:48 +0000

    First commit od Adafruit Huzzah pot. experiment

---
+++ potentiometer.py
@@ -0,0 +1,18 @@
+# Read a potentiometer using ADC on Adafruit Huzzah 
+
+import machine
+import time
+
+# Configure the pin connected to the middle of the potentiometer
+# For Adafruit Feather HUZZAH ESP8266, the analog pin is often A0
+# Pin 0 is A0
+
+adc = machine.ADC(0)
+
+fac=4*65535
+
+while True:
+    # Read the raw 16-bit value from the ADC
+    value = (adc.read_u16())
+    print(f"Potentiometer value: {value}")
+    time.sleep(0.1)
